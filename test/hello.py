9041daacf71d294bb049c976d6a93d5f189ca50c
qstar <davesla2012@hotmail.com>
2016-03-01 12:59:02 +0800

    Python socket server

---
+++ test/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# name = input('please enter your name: ')
+# print('hello,', name)
+
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+sum = 0
+for x in range(101):
+    sum += x
+print(sum)
